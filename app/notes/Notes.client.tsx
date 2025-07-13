3da3931ebfb0d475d9d1cd10b2aaaf76583ed1a0
Artem Kolesnyk <artemkolesnyk0201@gmail.com>
2025-07-13 22:12:16 +0100

    3

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useDebounce } from "use-debounce";
-import { fetchNotes, NotesResponse } from "@/lib/api";
+import { fetchNotes, NotesResponse, FetchNotesProps} from "@/lib/api";
 import css from "./NotesPage.module.css";
 import SearchBox from "@/components/SearchBox/SearchBox";
 import Pagination from "@/components/Pagination/Pagination";
